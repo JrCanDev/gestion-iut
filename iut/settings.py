c3c03c59ba6feac9c409ab0ee5ae6ec601d7be83
Samuel Delepoulle <samuel.delepoulle@univ-littoral.fr>
2022-04-13 14:59:18 +0200

    fix ALLOWED_HOSTS ?

--- iut/settings.py
+++ iut/settings.py
@@ -25,7 +25,7 @@ SECRET_KEY = 'django-insecure-5zr7#oaskt6cuek!e%ywepb#(&t400d$bn0u$d*&is5bx=k95w
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
 
-ALLOWED_HOSTS = []
+ALLOWED_HOSTS = ['127.0.0.1', 'localhost']
 
 
 # Application definition
